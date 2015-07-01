78bb76041dacc97a35c08cc829505d782350b71a
Gonti <goonti@gmail.com>
2015-07-01 15:06:43 +0200

    Update and rename MCTActionSheetHelper.h to MCTAlertHelper.h

---
+++ MCTAlertHelper.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface MCTAlertHelper : NSObject
+
+- (id)initWithViewController:(UIViewController *)controller cancelButtonTitle:(NSString *)cancel style:(UIAlertControllerStyle)style title:(NSString *)title message:(NSString *)message;
+- (id)initWithViewController:(UIViewController *)controller array:(NSArray *)sortedTitles dictionary:(NSDictionary *)blocksAndTitiles cancelButtonTitle:(NSString *)cancel style:(UIAlertControllerStyle)style title:(NSString *)title message:(NSString *)message;
+- (void)presentAlert;
+- (void)addTitle:(NSString *)title WithAction:(void (^)(UIAlertAction *action))blockAction;
+
+@end
